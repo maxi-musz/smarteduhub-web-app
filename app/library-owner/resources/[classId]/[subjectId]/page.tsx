0b8d5ceb6139a930addb2dd3ff68a8db3fbf521c
Maximus <bernardmayowaa@gmail.com>
2026-01-26 21:59:21 +0100

    fixed errors from build after fixing topic update not refetching

--- app/library-owner/resources/[classId]/[subjectId]/page.tsx
+++ app/library-owner/resources/[classId]/[subjectId]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useParams, useRouter } from "next/navigation";
 import { useLibrarySubjectTopics } from "@/hooks/library-owner/use-library-topics";
 import { useLibrarySubject } from "@/hooks/library-owner/use-library-subject";
